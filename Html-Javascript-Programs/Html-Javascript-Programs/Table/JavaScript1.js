65a94d488608eb5253d5ffcb7db1e418f07af6fe
baburajnish <baburajnish@gmail.com>
2017-02-19 00:22:04 +0530

    Add Table form

---
+++ Html-Javascript-Programs/Html-Javascript-Programs/Table/JavaScript1.js
@@ -0,0 +1,20 @@
+﻿
+function btn() {
+    var fName = document.getElementById('txtName');
+    fName.value = "Rajnish Tiwari";
+    var email = document.getElementById('txtEmail');
+    email.value = "baburajnish@gmail.com"
+    var password = document.getElementById('txtPwd');
+    password.value = "raj1234";
+    var dob = document.getElementById('txtDob');
+    dob.value = "22-08-1987";
+    var mob = document.getElementById('txtMob');
+    mob.value = "7205332706";
+    var Gender = document.getElementById('radioMale');
+    Gender.checked = true;
+    var Qualifiction = document.getElementById('checkGraduation');
+    Qualifiction.checked = true;
+    var Address = document.getElementById('txtArea');
+    Address.value = "Name-Rajnish Tiwari At-Bahadurpara,Brajrajnagar Ps-Brajrajnagar Dist-Jharsuguda Pin-768216 (ODISHA)";
+
+}
